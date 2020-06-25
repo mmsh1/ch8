973391a1621caf1f109f6d43d7a0d170ca78a15b
mm-sh <shiman93@mail.ru>
2020-06-25 15:59:37 +0300

    Main emulation cycle could not detect any keystokes due forgotten breaks. Fixed

--- sdl_layer.c
+++ sdl_layer.c
@@ -49,7 +49,11 @@ sdl_layer_init(const char *wname, int width, int height, int scale)
         SDL_Quit();
         return -1;
     }
-    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, width, height);
+    texture = SDL_CreateTexture(renderer,
+                                SDL_PIXELFORMAT_RGBA32,
+                                SDL_TEXTUREACCESS_STREAMING,
+                                width,
+                                height);
     if (!texture) {
         fprintf(stderr, "Error: texture creation failed. %s\n", SDL_GetError());
         SDL_DestroyRenderer(renderer);
@@ -71,107 +75,45 @@ sdl_handle_keystroke(uint8_t *keys, uint8_t *quit_flag)
                 break;
             case SDL_KEYDOWN: {
                 switch (event.key.keysym.sym) {
-                    case SDLK_1:
-                        keys[0] = 1;
-                        break;
-                    case SDLK_2:
-                        keys[1] = 1;
-                        break;
-                    case SDLK_3:
-                        keys[2] = 1;
-                        break;
-                    case SDLK_4:
-                        keys[3] = 1;
-                        break;
-                    case SDLK_q:
-                        keys[4] = 1;
-                        break;
-                    case SDLK_w:
-                        keys[5] = 1;
-                        break;
-                    case SDLK_e:
-                        keys[6] = 1;
-                        break;
-                    case SDLK_r:
-                        keys[7] = 1;
-                        break;
-                    case SDLK_a:
-                        keys[8] = 1;
-                        break;
-                    case SDLK_s:
-                        keys[9] = 1;
-                        break;
-                    case SDLK_d:
-                        keys[10] = 1;
-                        break;
-                    case SDLK_f:
-                        keys[11] = 1;
-                        break;
-                    case SDLK_z:
-                        keys[12] = 1;
-                        break;
-                    case SDLK_x:
-                        keys[13] = 1;
-                        break;
-                    case SDLK_c:
-                        keys[14] = 1;
-                        break;
-                    case SDLK_v:
-                        keys[15] = 1;
-                        break;
+                    case SDLK_1: keys[0] = 1; break;
+                    case SDLK_2: keys[1] = 1; break;
+                    case SDLK_3: keys[2] = 1; break;
+                    case SDLK_4: keys[3] = 1; break;
+                    case SDLK_q: keys[4] = 1; break;
+                    case SDLK_w: keys[5] = 1; break;
+                    case SDLK_e: keys[6] = 1; break;
+                    case SDLK_r: keys[7] = 1; break;
+                    case SDLK_a: keys[8] = 1; break;
+                    case SDLK_s: keys[9] = 1; break;
+                    case SDLK_d: keys[10] = 1; break;
+                    case SDLK_f: keys[11] = 1; break;
+                    case SDLK_z: keys[12] = 1; break;
+                    case SDLK_x: keys[13] = 1; break;
+                    case SDLK_c: keys[14] = 1; break;
+                    case SDLK_v: keys[15] = 1; break;
                 }
+                break;
             }
             case SDL_KEYUP: {
                 switch (event.key.keysym.sym) {
-                    case SDLK_1:
-                        keys[0] = 0;
-                        break;
-                    case SDLK_2:
-                        keys[1] = 0;
-                        break;
-                    case SDLK_3:
-                        keys[2] = 0;
-                        break;
-                    case SDLK_4:
-                        keys[3] = 0;
-                        break;
-                    case SDLK_q:
-                        keys[4] = 0;
-                        break;
-                    case SDLK_w:
-                        keys[5] = 0;
-                        break;
-                    case SDLK_e:
-                        keys[6] = 0;
-                        break;
-                    case SDLK_r:
-                        keys[7] = 0;
-                        break;
-                    case SDLK_a:
-                        keys[8] = 0;
-                        break;
-                    case SDLK_s:
-                        keys[9] = 0;
-                        break;
-                    case SDLK_d:
-                        keys[10] = 0;
-                        break;
-                    case SDLK_f:
-                        keys[11] = 0;
-                        break;
-                    case SDLK_z:
-                        keys[12] = 0;
-                        break;
-                    case SDLK_x:
-                        keys[13] = 0;
-                        break;
-                    case SDLK_c:
-                        keys[14] = 0;
-                        break;
-                    case SDLK_v:
-                        keys[15] = 0;
-                        break;
+                    case SDLK_1: keys[0] = 0; break;
+                    case SDLK_2: keys[1] = 0; break;
+                    case SDLK_3: keys[2] = 0; break;
+                    case SDLK_4: keys[3] = 0; break;
+                    case SDLK_q: keys[4] = 0; break;
+                    case SDLK_w: keys[5] = 0; break;
+                    case SDLK_e: keys[6] = 0; break;
+                    case SDLK_r: keys[7] = 0; break;
+                    case SDLK_a: keys[8] = 0; break;
+                    case SDLK_s: keys[9] = 0; break;
+                    case SDLK_d: keys[10] = 0; break;
+                    case SDLK_f: keys[11] = 0; break;
+                    case SDLK_z: keys[12] = 0; break;
+                    case SDLK_x: keys[13] = 0; break;
+                    case SDLK_c: keys[14] = 0; break;
+                    case SDLK_v: keys[15] = 0; break;
                 }
+                break;
             }
         }
     }
@@ -195,4 +137,5 @@ sdl_layer_destroy()
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window); /* TODO test on NULL */
+    SDL_Quit();
 }
