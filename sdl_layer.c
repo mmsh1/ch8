659cb45b22e2c8c57fcc3eba5bf8ae0dcf7744a9
mm-sh <shiman93@mail.ru>
2020-06-26 11:44:35 +0300

    correct keyboard mapping

--- sdl_layer.c
+++ sdl_layer.c
@@ -4,25 +4,6 @@ SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 SDL_Texture *texture = NULL;
 
-/*uint8_t keyboard[16] = {
-    SDLK_1,
-    SDLK_2,
-    SDLK_3,
-    SDLK_4,
-    SDLK_q,
-    SDLK_w,
-    SDLK_e,
-    SDLK_r,
-    SDLK_a,
-    SDLK_s,
-    SDLK_d,
-    SDLK_f,
-    SDLK_z,
-    SDLK_x,
-    SDLK_c,
-    SDLK_v
-};*/
-
 int
 sdl_layer_init(const char *wname, int width, int height, int scale)
 {
@@ -75,42 +56,48 @@ sdl_handle_keystroke(uint8_t *keys, uint8_t *quit_flag)
                 break;
             case SDL_KEYDOWN: {
                 switch (event.key.keysym.sym) {
-                    case SDLK_1: keys[0] = 1; break;
-                    case SDLK_2: keys[1] = 1; break;
-                    case SDLK_3: keys[2] = 1; break;
-                    case SDLK_4: keys[3] = 1; break;
+                    case SDLK_1: keys[1] = 1; break;
+                    case SDLK_2: keys[2] = 1; break;
+                    case SDLK_3: keys[3] = 1; break;
+                    case SDLK_4: keys[12] = 1; break;
+
                     case SDLK_q: keys[4] = 1; break;
                     case SDLK_w: keys[5] = 1; break;
                     case SDLK_e: keys[6] = 1; break;
-                    case SDLK_r: keys[7] = 1; break;
-                    case SDLK_a: keys[8] = 1; break;
-                    case SDLK_s: keys[9] = 1; break;
-                    case SDLK_d: keys[10] = 1; break;
-                    case SDLK_f: keys[11] = 1; break;
-                    case SDLK_z: keys[12] = 1; break;
-                    case SDLK_x: keys[13] = 1; break;
-                    case SDLK_c: keys[14] = 1; break;
+                    case SDLK_r: keys[13] = 1; break;
+
+                    case SDLK_a: keys[7] = 1; break;
+                    case SDLK_s: keys[8] = 1; break;
+                    case SDLK_d: keys[9] = 1; break;
+                    case SDLK_f: keys[14] = 1; break;
+
+                    case SDLK_z: keys[10] = 1; break;
+                    case SDLK_x: keys[0] = 1; break;
+                    case SDLK_c: keys[11] = 1; break;
                     case SDLK_v: keys[15] = 1; break;
                 }
                 break;
             }
             case SDL_KEYUP: {
                 switch (event.key.keysym.sym) {
-                    case SDLK_1: keys[0] = 0; break;
-                    case SDLK_2: keys[1] = 0; break;
-                    case SDLK_3: keys[2] = 0; break;
-                    case SDLK_4: keys[3] = 0; break;
+                    case SDLK_1: keys[1] = 0; break;
+                    case SDLK_2: keys[2] = 0; break;
+                    case SDLK_3: keys[3] = 0; break;
+                    case SDLK_4: keys[12] = 0; break;
+
                     case SDLK_q: keys[4] = 0; break;
                     case SDLK_w: keys[5] = 0; break;
                     case SDLK_e: keys[6] = 0; break;
-                    case SDLK_r: keys[7] = 0; break;
-                    case SDLK_a: keys[8] = 0; break;
-                    case SDLK_s: keys[9] = 0; break;
-                    case SDLK_d: keys[10] = 0; break;
-                    case SDLK_f: keys[11] = 0; break;
-                    case SDLK_z: keys[12] = 0; break;
-                    case SDLK_x: keys[13] = 0; break;
-                    case SDLK_c: keys[14] = 0; break;
+                    case SDLK_r: keys[13] = 0; break;
+
+                    case SDLK_a: keys[7] = 0; break;
+                    case SDLK_s: keys[8] = 0; break;
+                    case SDLK_d: keys[9] = 0; break;
+                    case SDLK_f: keys[14] = 0; break;
+
+                    case SDLK_z: keys[10] = 0; break;
+                    case SDLK_x: keys[0] = 0; break;
+                    case SDLK_c: keys[11] = 0; break;
                     case SDLK_v: keys[15] = 0; break;
                 }
                 break;
